282ad97ce76b749ff70b29dadd8cbaa77ea023a5
njagua <njagua.gma@gmail.com>
2025-01-08 12:57:11 +0300

    Intro to git and github

---
+++ Git_&_Github.js
@@ -0,0 +1,71 @@
+/* 
+A version control system lets you track your changes to the source code.
+
+Git: version control system for tracking changes in code $ collab with others
+     - serves as a central point for collaboration with a focus on agile methodologies
+
+Github: Is an online hosting platform for Git repositories $ collaborate with others, share code, and manage projects.
+
+Git supports featured branching
+
+What is so special about Git? /Features of git repository model
+1: tracks source code
+2: provides a distributed version control system - you can revert changes
+
+Git and Github : Basic terms
+
+SSH protocol: Method to secure remote login
+
+Repository: a data structure for storing docs including source codes
+          :  contains project folders
+          : a repo can track & maintain version control
+
+Fork: copy of a repository
+
+Pull request: to request for review and approval of changes
+
+Working directory: contains files and subdirectories
+
+commit: snapshot of the projects current state along with description of changes made
+
+Branch: Separate line of development that allows you to work on features or fixes independently
+
+Merge: Combining changes from one branch into another
+
+Clone: Local copy of the remote repository
+
+Gitlab: A DevOps platform that:
+   - supports CI/CD pipelines & streamline testing
+   - provides access to git repositories
+   - allows devs to collaborate
+   -enables devs to work from a local copy
+   -enables devs to branch and merge code
+
+Distributed version control system (DVCS): A system that keeps track of changes to code, regardless of where it is stored.
+
+Continuous delivery (CD): The automated movement of software through the software development lifecycle.
+
+Continuous integration (CI): The process of automatically building and testing code to ensure it is working correctly.
+
+   Github Branches
+
+Branches store all files in github
+Main branch store the deployable code
+Create a new branch for planned changes/ to develop new features
+
+Commit best practices:
+-Do not end commit message with a period
+-use less than 50 characters
+
+Pull Requests
+- makes the proposed/committed changes available for others to review and use
+-Github auto makes a pull request if you make a change on a branch you dont own
+- A pull request is a way to notify other team members of the changes and edits made to the main branch.
+
+What is the purpose of a license?
+-To express how people can use your code
+
+What happens if you make a change on a repository branch that you don't own?
+- GitHub generates an error message.
+
+*/
